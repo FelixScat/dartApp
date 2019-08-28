ea3381fcfd3e78571a205f82a89637389836050f
Felix <zyhzhangyunhao@gmail.com>
2019-08-28 19:02:23 +0800

    feat: test

--- bin/main.dart
+++ bin/main.dart
@@ -1,7 +1,5 @@
 import 'package:dartApp/dartApp.dart' as dartApp;
 
 main(List<String> arguments) {
-  // print('Hello world: ${dartApp.calculate()}!');
-  final name = "zhangsan";
-  print(name);
+  print(DateTime.now().millisecondsSinceEpoch);
 }
